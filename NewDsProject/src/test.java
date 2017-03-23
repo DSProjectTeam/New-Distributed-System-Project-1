c9cd7a88a1fa40d760a9a811f32fcfd82135a08c
ZizheRuan <ruanzizhe@live.com>
2017-03-24 10:40:29 +1100

    22
    22

--- NewDsProject/src/test.java
+++ NewDsProject/src/test.java
@@ -7,19 +7,16 @@ public class test {
 		System.out.println("this line write by the master");
 		
 		System.out.println("change by my own branch");
+		System.out.println("change by my own branch");
 		//6666666
 		///asdfsadfsdaf
 		//new changes byu tester
 		//Hello!NiceToMeetYouAll!
 		//finally it is working! cheers guys!
-<<<<<<< HEAD
+
 		
 		
-		//hello
-		
-=======
-		//jhhhhj
->>>>>>> origin/master
+
 
 	}
 
