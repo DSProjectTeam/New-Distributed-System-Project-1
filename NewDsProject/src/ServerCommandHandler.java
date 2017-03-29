e43a374fcd8e0124578d4fbd097c093251c17f18
swatghc <447099673@qq.com>
2017-03-30 01:05:12 +1100

    I start to write the server.

---
+++ NewDsProject/src/ServerCommandHandler.java
@@ -0,0 +1,17 @@
+
+public class ServerCommandHandler {
+	
+	
+	public static void handlingPublish(Resource resource){
+		
+	}
+	
+	public static void handlingRemove (Resource resource){
+		
+	}
+	
+	public static void HandlingShare (Resource resource, String secret){
+		
+	}
+
+}
