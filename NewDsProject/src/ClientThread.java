0431ffb03497e7116bb289e3db26b084f27df8e8
ZizheRuan <ruanzizhe@live.com>
2017-04-05 22:13:03 +1000

    加入了两个新文件
    加入ClientHandler和ClientThread两个文件

---
+++ NewDsProject/src/ClientThread.java
@@ -0,0 +1,126 @@
+package dsdemo;
+
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.net.Socket;
+import java.util.ArrayList;
+import java.util.HashMap;
+
+import org.json.JSONObject;
+import org.json.simple.parser.JSONParser;
+import org.json.simple.parser.ParseException;
+
+public class ClientThread extends Thread{
+	Socket clientSocket;
+
+	private BufferedReader input;
+	
+	private BufferedWriter output;
+	
+	
+	
+	public ClientThread(Socket socket){
+		try {
+			this.serverSocket = socket;
+			this.output = new BufferedWriter(new OutputStreamWriter(clientSocket.getOutputStream()));
+			this.input = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()));
+			
+		} catch (IOException e) {
+			if(serverSocket!=null){
+				try {
+					serverSocket.close();
+				} catch (IOException e2) {
+					// TODO: handle exception
+				}
+			}
+		}
+	}
+
+
+	
+	
+	
+	public void getReadyAndSend(String args[]){
+	
+		String command = args[0];
+		switch (command){//要考虑command为空的错误情况
+		case -publish:
+			JSONObject publish = new JSONObject();
+			publish.put("command", command);//command等的值均来自ClientHandler.java中command的值
+			publish.put("name",name);
+			publish.put("tags",tags);//注意格式
+			publish.put("description",description);
+			publish.put("uri",uri);
+			publish.put("channel",name);
+			publish.put("owner",owner);
+			publish.put("ezserver",ezserver);
+			break;
+			
+		case -remove:
+			break;
+			
+		default: break;
+		}
+	}
+	
+
+		try {//不会写哭哭😢 >...<
+			out.write("?");
+			out.flush();
+		} catch (IOException e) {
+			
+		}
+	
+	
+	
+	
+	
+	
+	
+	
+	public void handleResponse (String string){
+		JSONParser parser = new JSONParser();
+		JSONObject jsonObject;
+		try {
+			jsonObject = (JSONObject) parser.parse(string);
+			ConstantEnum.CommandType command  = ConstantEnum.CommandType.valueOf((String)jsonObject.get("command"));
+			
+			switch (command) {
+			case debug:	
+				break;
+			case publish:
+				String resoponse = (String) jsonObject.get("resoponse");
+				String errorMessage = (String) jsonObject.get("errorMessage");
+				System.out.println("{\"response\" : "+response+"\",\n\"errorMessage\" : \""+errorMessage+"\"}");
+				break;
+			case remove:
+				break;
+			case share:
+				break;
+			case fetch:
+				
+				break;
+			case query:
+				
+				break;
+			case exchange:
+					
+				break;
+			default:
+				break;
+			}
+		} catch (ParseException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		
+	}
+	
+	
+	
+	
+	
+}
